3f5ee39ed8f8e8b1dbd271bcea3614ed5735cd98
Huangweize <126.com>
2026-01-31 01:13:26 +0800

    本地化照片资源

--- constants.ts
+++ constants.ts
@@ -1,19 +1,19 @@
 export const PRESET_PERSON_IMAGES = [
-  "https://i.mji.rip/2025/11/19/51362f10bf363524251691516b323f46.png",
-  "https://i.mji.rip/2025/11/19/d42af8d2f41175683d1467bc2c857cbe.png",
-  "https://p2.itc.cn/q_70/images03/20230712/dfb1cb57f0d045db86efe369415877cf.jpeg",
-  "https://i.mji.rip/2025/11/19/665c90d26a3ec857c5c440f4f7da5ed2.png",
-  "https://i.mji.rip/2025/11/19/15f1393ca6e0215caf51cf0ecdfe0d5c.png",
-  "https://img-s.msn.cn/tenant/amp/entityid/AA1GC7a3.img?w=520&h=780&m=6&x=222&y=183&s=140&d=140"
+  "/images/person/person1.png",
+  "/images/person/person2.png",
+  "/images/person/person3.jpeg",
+  "/images/person/person4.png",
+  "/images/person/person5.png",
+  "/images/person/person6.jpg"
 ];
 
 export const PRESET_CLOTHES_IMAGES = [
-  "https://i.mji.rip/2025/11/19/17aab709b470423cc3e89772afb7da39.png",
-  "https://i.mji.rip/2025/11/19/cd8598d0d987b7b5825257924be13bcb.png",
-  "https://i.mji.rip/2025/11/19/32426d416cb5508300c530a071ebe497.png",
-  "https://i.mji.rip/2025/11/19/6bd78dd58ee4e038e219145822b356e6.png",
-  "https://i.mji.rip/2025/11/19/e6dccb4533c546cb42fa564b322d8e84.png",
-  "https://i.mji.rip/2025/11/19/a838237484463bfe6ce45a8245587048.png"
+  "/images/clothes/clothes1.png",
+  "/images/clothes/clothes2.png",
+  "/images/clothes/clothes3.png",
+  "/images/clothes/clothes4.png",
+  "/images/clothes/clothes5.png",
+  "/images/clothes/clothes6.png"
 ];
 
 export const MODEL_NAME = 'gemini-2.5-flash-image';
